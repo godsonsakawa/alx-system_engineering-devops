9236830c086a20aba325fb07d2d896f81f7a9c37
godson sakawa <godsonsakawa@gmail.com>
2023-04-24 23:46:12 +0300

    Test: installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,8 +8,8 @@ package { $packages:
 }
 
 # Install flask version 2.1.0 with pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  require => Package['python3-pip']
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip']
 }
